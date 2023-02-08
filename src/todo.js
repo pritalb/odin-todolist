bcd12e41aeea4bbf6445c66a94f9ec20834e5ea6
pritalb <pritalbamnodkar@gmail.com>
2023-02-08 16:42:14 +0530

    taskfactory now uses a separate task function to create tasks and acts as an abstraction

--- src/todo.js
+++ src/todo.js
@@ -1,4 +1,4 @@
-function taskFactory (title, description, duedate, priority) {
+function task (title, description, duedate, priority) {
     let finished = false;
     const done = () => {
         finished = true;
@@ -11,6 +11,15 @@ function taskFactory (title, description, duedate, priority) {
     return {
         title, description, duedate, priority, checkIfTaskDone, done
     };
+}
+
+function taskFactory (title, description, duedate, priority) {
+    if (typeof(title) === "string" && typeof(description) === "string" && typeof(priority) === "string" && typeof(duedate) === "object") {
+        return task(title, description, duedate, priority);
+    }
+    
+    console.error('Wrong argument types! make sure title, description and priority are strings and duedate a Date object');
+    return null;
 }
 
 export { taskFactory, };
