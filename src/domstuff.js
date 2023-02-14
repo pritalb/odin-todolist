54c7edf1b0f1dd5e0c71c0f7ef62e0f156647652
pritalb <pritalbamnodkar@gmail.com>
2023-02-14 15:19:31 +0530

    implemented function for creating dom element for a task

--- src/domstuff.js
+++ src/domstuff.js
@@ -1,7 +1,38 @@
+function createTaskContainer (task) {
+    let container = document.createElement('div');
+
+    let title = document.createElement('div');
+    title.className = 'task-title';
+    title.innerText = `${task.title}`;
+
+    let description = document.createElement('div');
+    description.className = 'task-description';
+    description.innerText = `${task.description}`;
+
+    let duedate = document.createElement('div');
+    duedate.className = 'task-duedate';
+    duedate.innerText = `${task.duedate}`;
+
+    let priority = document.createElement('div');
+    priority.className = 'task-priority';
+    priority.innerText = `${task.priority}`;
+    // title, description, due date and priority
+
+    container.appendChild(title);
+    container.appendChild(description);
+    container.appendChild(duedate);
+    container.appendChild(priority);
+    
+    return container;
+}
+
+
 function outputTask (task) {
     console.log(
         `title: ${task.title}, description: ${task.description}, due date: ${task.duedate}, priority: ${task.priority}, done?: ${task.checkIfTaskDone()}`
     );
+    // let taskContainer = createTaskContainer(task);
+    // document.querySelector('#main').appendChild(taskContainer);
 }
 
 function outputProjectTasks (project) {
@@ -14,4 +45,4 @@ function outputProjectTasks (project) {
     };
 }
 
-export { outputTask, outputProjectTasks };
+export { outputTask, outputProjectTasks, createTaskContainer, };
--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
 import { projectsContainer, deleteTask } from "./storagestuff";
 import { taskFactory, projectFactory } from "./todo";
-import { outputTask, outputProjectTasks, getCurrentProject } from "./domstuff";
+import { outputTask, outputProjectTasks, createTaskContainer, } from "./domstuff";
 
 function main() {
     const main_container = document.createElement('div');
     let project1 = projectFactory('project1');
     let defaultProject = projectFactory('default');
     let current_project = projectsContainer.getCurrentProjectObject();
-    // console.log(current_project);
-    // console.log(getCurrentProject());
 
     let task1 = taskFactory('title 1', 'some description 1', new Date(2023, 4, 16), 'low');
-    let task2 = taskFactory('title 2', 'some description 2', new Date(2023, 5, 16), 'high');
-    let task3 = taskFactory('title 3', 'some description 3', new Date(2023, 5, 16), 'high', 'project1');
-    // outputTask(task);
-
-    // defaultProject.addTask(task1);
-    // // defaultProject.addTask(task2);
-    // current_project.addTask(task2);
-    // defaultProject.addTask(task3);
+    let task1DOM = createTaskContainer(task1);
+    main_container.appendChild(task1DOM);
+ 
     outputProjectTasks(current_project);
 
-    console.log('after deleting a task');
-    // deleteTask(task1.title);
-    current_project.editTask(task2.title ,'edited title 2', 'new and improved description 2', new Date(2023, 5, 16), 'high')
-    // outputProjectTasks(defaultProject);
-
-    console.log(current_project.getAllTasks());
-    console.log(projectsContainer.getProject('project1').getAllTasks());
-    // console.log(projectsContainer.getAllProjects());
     return main_container;
 }
 
